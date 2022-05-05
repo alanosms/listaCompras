a9c14af447867ea0d31a875156c709c13a93f70f
Alan S. Martins <50818058+alanosms@users.noreply.github.com>
2022-05-05 18:27:33 -0300

    feat/fix(table): responsible table with js

--- listarCompras.js
+++ listarCompras.js
@@ -2,6 +2,9 @@ const nameProduct = document.getElementById("nameProduct");
 const priceProduct = document.getElementById("priceProduct");
 const tableProducts = document.getElementById("tableProducts");
 
+const clearall = document.getElementById("clearall");
+
+let values = [];
 
 const totalDiv = document.getElementById("totalDiv");
 let divResult = document.getElementById("result");
@@ -9,31 +12,54 @@ let totalPurchases = 0;
 
 let btnadd = document.getElementById("addProduct");
 
-btnadd.addEventListener('click', captureValues);
+btnadd.addEventListener("click", captureValues);
+clearall.addEventListener("click", clearAllList);
+function testaction() {
+  console.log("OK");
+}
+function captureValues() {
+  let inputNameProduct = nameProduct.value;
+  let inputPriceProduct = parseFloat(priceProduct.value);
+  if (inputNameProduct == "" || inputPriceProduct == "") {
+    alert("Valores Invalido");
+    return;
+  }
+  values.push({ name: inputNameProduct, price: inputPriceProduct });
+  nameProduct.value = "";
+  priceProduct.value = "";
 
-function captureValues(){
-    let inputNameProduct = nameProduct.value;
-    let inputPriceProduct = parseFloat(priceProduct.value);
-    if (inputNameProduct == "" || inputPriceProduct == "") {
-        alert("Valores Invalido")
-        return;
-    }
-    addRow(inputNameProduct, inputPriceProduct); 
-    addTotal(inputPriceProduct);
-    nameProduct.value = "";
-    priceProduct.value = "";
+  values.forEach((el) => {
+    totalPurchases = totalPurchases + el.price;
+    console.log(el.price);
+  });
+  divResult.innerHTML = totalPurchases;
+  totalPurchases = 0;
+  addRows();
 }
-function addTotal(inputPriceProduct){
-    divResult.innerHTML = totalPurchases += inputPriceProduct;
-    console.log("Compras: "+totalPurchases);
-}
-function addRow(inputNameProduct, inputPriceProduct){
+
+function addRows() {
+  clearList();
+  values.forEach((el) => {
+    let rowCount = document.getElementById("tableProducts").rows.length;
+    if (rowCount > 1) document.getElementById("tableProducts").deleteRow(1);
+  });
+  values.forEach((el) => {
     let tableRow = tableProducts.insertRow();
     let nomeproduto = tableRow.insertCell();
     let precoproduto = tableRow.insertCell();
 
-    nomeproduto.innerHTML = inputNameProduct;
-    precoproduto.innerHTML = inputPriceProduct;
+    nomeproduto.innerHTML = el.name;
+    precoproduto.innerHTML = el.price;
+  });
 }
-
-
+function clearAllList() {
+  clearList();
+  values = [];
+  alert("OK. lista apagada");
+}
+function clearList() {
+  values.forEach((el) => {
+    let rowCount = document.getElementById("tableProducts").rows.length;
+    if (rowCount > 1) document.getElementById("tableProducts").deleteRow(1);
+  });
+}
