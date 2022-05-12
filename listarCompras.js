c5f6d9ccaac8fabd38d6f583dfa11cb2efe952ea
Alan S. Martins <50818058+alanosms@users.noreply.github.com>
2022-05-12 00:38:22 -0300

    fix(delete): fixing delete
    fix delete method, problems searching and trying to delete

--- listarCompras.js
+++ listarCompras.js
@@ -4,7 +4,9 @@ const tableProducts = document.getElementById("tableProducts");
 
 const clearall = document.getElementById("clearall");
 
+
 let values = [];
+let increment = 0;
 
 const totalDiv = document.getElementById("totalDiv");
 let divResult = document.getElementById("result");
@@ -29,41 +31,67 @@ function captureValues() {
     alert("Valores Invalido");
     return;
   }
-  values.push({ name: inputNameProduct, price: inputPriceProduct });
+  const product ={
+    id: increment,
+    name: inputNameProduct, 
+    price: inputPriceProduct
+  }
+
+  values.push(product);
+
   nameProduct.value = "";
   priceProduct.value = "";
+  increment = increment + 1;
 
   values.forEach((el) => {
     totalPurchases = totalPurchases + el.price;
   });
+
   divResult.innerHTML = totalPurchases;
   totalPurchases = 0;
   addRows(values);
 }
+
 function addRows(arrayInput) {
   clearList();
+
   arrayInput.forEach((el) => {
     let rowCount = document.getElementById("tableProducts").rows.length;
     if (rowCount > 1) document.getElementById("tableProducts").deleteRow();
   });
+
   arrayInput.forEach((el) => {
     let tableRow = tableProducts.insertRow();
+    let idProductValue = tableRow.insertCell();
     let nameProductValue = tableRow.insertCell();
     let priceProductValue = tableRow.insertCell();
     let removeProduct = tableRow.insertCell();
 
+    tableRow.id = `idRow${el.id}`;
+    
+    idProductValue.innerHTML = el.id;
     nameProductValue.innerHTML = el.name;
     priceProductValue.innerHTML = el.price;
-    removeProduct.innerHTML = "<button onclick='deleteRow(this)'>X</button>";
+    removeProduct.innerHTML = `<button onclick="deleteRow(${el.id})">X</button>`;
   });
 }
-function deleteRow(r) {
-  var i = r.parentNode.parentNode.rowIndex;
-  let indexProduct = i - 1;
-  document.getElementById("tableProducts").deleteRow(i);
-  divResult.innerHTML = divResult.innerHTML - values[indexProduct].price;
-  values.splice(indexProduct, 1);
+function deleteRow(id) {
+
+  const row = document.getElementById(`idRow${id}`);
+  row.parentNode.removeChild(row);
+
+  const productIndex = values.findIndex(product => product.id === id);
+
+  divResult.innerHTML = divResult.innerHTML - values[id].price;
+  values.splice(productIndex, 1);
+  console.log("indexProduct: ", productIndex);
 }
+
+function checkID(){
+  const index = values.findIndex(el => el.id == comp);
+  return index;
+  }
+
 function clearAllList() {
   clearList();
   values = [];
@@ -86,6 +114,7 @@ function search() {
     if (myMatch) {
       arrayMatchs.push(el);
     }
+
   });
   if (arrayMatchs.length > 0){
      addRows(arrayMatchs);
